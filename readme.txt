623bcd5046d5d8a41174b1a54a0785a930e2671f
mara <ms.marafinkel@gmail.com>
2021-07-13 15:31:15 -0700

    Make confetti not regenerate

---
+++ readme.txt
@@ -0,0 +1,10 @@
+confetti: https://www.npmjs.com/package/confetti-js
+arctic map: https://external-preview.redd.it/KgwsiCKf1mkDFneXMf4qItNTyAAW0b4LeDxfkTjyCag.jpg?width=960&crop=smart&auto=webp&s=f2eeaf7690ee7ded97042ecfc3494171b6f9937e
+desert map: https://cdn1.epicgames.com/ue/product/Screenshot/UnrealFeatured%20%20227-1920x1080-17254464e38a2f8a26fc1928b01ede97.png?resize=1&w=1600
+forest map: https://cdn1.epicgames.com/ue/product/Screenshot/HighresScreenshot00011-1920x1080-640c7b936d800d488822a47e39c64ac1.jpg?resize=1&w=1600
+grassland: https://www.treehugger.com/thmb/X3bHJaiEvY_fenAmbWl6sw_FkxI=/3906x2552/filters:fill(auto,1)/buffalo-gap-national-grasslands-56d40df05f9b5879cc8e69ac.jpg
+hill: https://wallup.net/wp-content/uploads/2015/06/Fantasy-hills-and-horses.jpg
+swamp: https://wallpaperaccess.com/full/2103209.jpg
+underdark: https://i.pinimg.com/originals/d5/be/65/d5be65505072561ab29cd3efa79033a1.jpg
+underwater map: https://i.pinimg.com/originals/9a/d0/93/9ad0938e7479fb9b016c0180b835e02b.png
+urban map: https://i.pinimg.com/originals/51/9e/4c/519e4c6e90390ef539bf72fa4c477efb.jpg
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+import monsters from '../dist/monsters.js';
+import magicItems from '../dist/magic_items.js';
+import ConfettiGenerator from "confetti-js";
+
 document.addEventListener('DOMContentLoaded', () => {
   const firstNames = ["Wiggle", "Tofu", "Bon Bon", "Bunny", "Crabcake", "Data", "Bad Lil Kitty", "Dad Body", "Hot Girl Summer", "Dumplin'", "Macho Man", "White Claw", "Flapjack", "Daddy", "Kitty Mike", "LeBron", "Queen", "LeBronda", "Demetrius", "Roly Poly", "Shmoo", "Sloppy Joe", "Sticky", "Stinky", "Tribbler", "Cool Ranch", "Tutti Fruiti", "Wicket", "Tomoko", "Yung", "Soup", "Charlie", "Veronika", "Jellybean", "Mara", "Jenny", "Trustin", "Mishka", "Mad Dog", "Mike Le", "Nicha", "MegaHam", "Caanburke", "Zeke", "Hilal", "Enchilada", "Walker", "Charis", "Jaycee", "Kevin", "Kiet", "Luca", "Barbarella", "Oscar", "Samwise", "Shirley", "Yun", "James Charles", "LaToya", "Zamin"]
-  const lastNames = ["Chesterton", "Finkel", "McSprinkle", "Cold One With The Boys", "Bernie Sanders", "OKCupid", "Tinder", "Bumble", "Hinge", "Plenty of Fish", "Match.com", "Men are Trash", "Epstein", "Love Isn't Real", "Flat Earth", "The Lord", "Furry Sanders", "McGee", "The Terminator", "Weed", "The Rock Johnson", "Fireball", "Randy Savage", "Destroyer of Worlds", "Never Forget", "Girlboss", "ADHD", "Flava Flav", "Daddy Maddy", "Lexapro", "Adderall", "Zoloft", "Baja Blasted", "Chris Harrison", "Kevin Federline", "Huff Sharpies", "Cry Evertim", "Based", "Gravy", "Bloodborne", "Southern Comfort", "Madsen", "Pilipenko", "Dell'Acqua", "Le", "Yang", "Chai", "Varli", "Ingebretson", "Magpusao", "Angcla", "Dance Dance Revolution", "Thank You Satan", "Thongpanchang", "Palacios Vazquez","Yoon", "Tang", "Kugshia", "Trader Joe's", "Cleveland, Ohio", "Sour Diesel", "Northern Lights", "Girl Scout Cookies", "Paul Wall", "Brook Hogan", "Acab", "Balloons", "Hallelujah Island", "Candy", "Marx Was Right", "My Parents Are Divorced", "Juicy Bugs"]
+  const lastNames = ["Chesterton", "Finkel", "McSprinkle", "Cold One With The Boys", "Bernie Sanders", "OKCupid", "Tinder", "Bumble", "Hinge", "Plenty of Fish", "Match.com", "Men are Trash", "Epstein", "Love Isn't Real", "Flat Earth", "The Lord", "Furry Sanders", "McGee", "The Terminator", "Weed", "The Rock Johnson", "Fireball", "Randy Savage", "Destroyer of Worlds", "Never Forget", "Girlboss", "ADHD", "Flava Flav", "Daddy Maddy", "Lexapro", "Adderall", "Zoloft", "Baja Blasted", "Chris Harrison", "Kevin Federline", "Huff Sharpies", "Cry Evertim", "Based", "Gravy", "Bloodborne", "Southern Comfort", "Madsen", "Pilipenko", "Dell'Acqua", "Le", "Yang", "Chai", "Varli", "Ingebretson", "Magpusao", "Angcla", "Dance Dance Revolution", "Thank You Satan", "Thongpanchang", "Palacios Vazquez","Yoon", "Tang", "Trader Joe's", "Cleveland, Ohio", "Sour Diesel", "Northern Lights", "Girl Scout Cookies", "Paul Wall", "Brook Hogan", "Acab", "Balloons", "Hallelujah Island", "Candy", "Marx Was Right", "My Parents Are Divorced", "Juicy Bugs"]
   const firstTavernNames = ["Adorable", "Aggressive", "Exactly, Exactly", "Anxious", "Bloody", "Blushing", "Crazy", "Dead", "Dizzy", "Interesting", "Easy", "Famous", "Fantastic", "Chafing", "Flying", "Missing",  "Friendly", "Gleaming", "Grieving", "Grumpy", "Happy", "Homely", "Impossible", "Jolly", "Lonely", "Muddy", "Mushy", "Naughty", "Nervous", "Pleasant", "Puzzled", "Scary", "Sparkling", "Splendid", "Stormy", "Tasty", "Tough", "Ugly", "Vivacious", "Weary", "Witty", "Zealous", "Wrong", "Daring", "Fleeing", "Lying", "Listening", ]
   const lastTavernNames = ["walrus", "dingo", "yak", "Freak of Nature", "Weirdo", "Pinata", "Redneck", "Bourgeoisie", "Alcoholic", "Divorcee", "hog", "gopher", "opossum", "jaguar", "squirrel", "leopard", "baboon", "alpaca", "mink", "skunk", "puma", "turtle", "snake", "hartebeest", "rabbit", "seal", "shrew", "ferret", "capybara", "hippopotamus", "kitten", "rat", "badger", "puppy", "koala", "basilisk", "wombat", "kangaroo", "crow", "ox", "gila Monster", "burro", "donkey", "salamander", "lion", "rhinoceros", "pronghorn"]
   const tavernTemperment = ["Surprised", "Stressed", "Mad", "Milky", "Loved", "Lonely", "Weird", "Uncomfortable", "Touched", "Tired", "Thankful", "Sympathetic", "Smart", "Sleepy", "Silly", "Pleased", "Pessimistic", "Sick", "Shocked", "Satisfied", "Sad", "Rushed", "Restless", "Relieved", "Relaxed", "Rejuvenated", "Rejected", "Refreshed", "Recumbent", "Quixotic", "Predatory", "Peaceful", "Optimistic", "Okay", "Numb", "Giggly", "Giddy", "Not specified", "Nerdy", "Infuriated", "Indifferent", "Indescribable", "Naughty", "Morose", "Moody", "Mischievous", "Mellow", "Melancholy", "Listless", "Lethargic", "Lazy", "Jubilant", "Jealous", "Irritated", "Irate", "Impressed", "Hyper", "Hungry", "Hot", "Hopeful", "Happy", "Guilty", "Grumpy", "Groggy", "Grateful", "Dorky", "Ditzy", "Discontent", "Good", "Gloomy", "Geeky", "Full", "Frustrated", "Flirty", "Exhausted", "Excited", "Enraged", "Energetic", "Anxious", "Annoyed", "Angry", "Ecstatic", "Drunk", "Drained", "Disappointed", "Dirty", "Devious", "Determined", "Depressed", "Dark", "Cynical", "Curious", "Alone", "Aggravated", "Accomplished", "Accepted", "Crushed", "Crazy", "Crappy", "Cranky", "Content", "Confused", "Complacent", "Cold", "Chipper", "Cheerful", "Calm", "Bouncy", "Bored", "Blissful", "Blank", "Blah", "Bittersweet", "Bewildered", "Awake", "Ashamed", "Apathetic", "Amused", "Exanimate", "Envious", "Enthralled", ]
   const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
   const randomNPCPic = new Array();
-  randomNPCPic[0] = "images/NPCs/machoman.jpeg";
+  randomNPCPic[0] = "images/NPCs/macho.jpg";
   randomNPCPic[1] = "images/NPCs/legolashotpic.jpg";
   randomNPCPic[2] = "images/NPCs/bret.jpg";
   randomNPCPic[3] = "images/NPCs/paula.jpg";
@@ -17,7 +21,10 @@ document.addEventListener('DOMContentLoaded', () => {
   randomNPCPic[7] = "images/NPCs/brendan.jpg";
   randomNPCPic[8] = "images/NPCs/pedro.jpg";
   randomNPCPic[9] = "images/NPCs/dane.jpg";
-
+  randomNPCPic[10] = "images/NPCs/NormMcDonald.jpg";
+  randomNPCPic[11] = "images/NPCs/TimHorton.jpg";
+  randomNPCPic[12] = "images/NPCs/jamie.jpg";
+  randomNPCPic[13] = "images/NPCs/trace.jpg";
 
   const randomTavern = new Array();
   randomTavern[0] = "images/Taverns/Tavern1.jpg";
@@ -47,8 +54,37 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('generated-text').innerText = generateTavern();
   }
 
-  // const background = new Array();
-  // background[0] = ""
+  const getRandomMonster = () => {
+    document.getElementById('generated-text').innerText = generateMonster();
+  }
+
+  const getRandomItem = () => {
+    document.getElementById('generated-text').innerText = generateItem();
+  }
+
+  const confetti = () => {
+    document.getElementById('my-canvas').innerText = generateConfetti();
+  }
+
+  const generateMonster = () => {
+    let idx = getRandomNumber(monsters.length);
+    fetch(`https://www.dnd5eapi.co${monsters[idx]['url']}`).then(function (response) {
+      // The API call was successful!
+      console.log('response!', response.json().type);
+      // getMonsterType
+    }).catch(function (err) {
+      // There was an error
+      console.warn('Something went wrong.', err);
+    });
+    return `Name: ${monsters[idx]['name']}`
+  }
+
+  const generateItem = () => {
+    let idx = getRandomNumber(magicItems.length);
+    return `Name: ${magicItems[idx]['name']}
+    Learn More: https://www.dnd5eapi.co${magicItems[idx]['url']}`;
+    
+  }
 
   const generateNPC = () => {
     getRandomImage();
@@ -63,11 +99,26 @@ document.addEventListener('DOMContentLoaded', () => {
     Temperment: ${tavernTemperment[getRandomNumber(tavernTemperment.length)]}`;
   }
 
+  const generateConfetti = () => {
+    var confettiSettings = { target: 'my-canvas', max: 2000, clock: 50, start_from_edge: true, rotate: true, respawn: false };
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+  }
+
   document.getElementById('npc')
     .addEventListener('click', setRandomName);
 
   document.getElementById('location')
     .addEventListener('click', setRandomTavernName);
+
+  document.getElementById('monster')
+    .addEventListener('click', getRandomMonster);
+
+  document.getElementById('magic_item')
+    .addEventListener('click', getRandomItem);
+
+  document.getElementById('surprise')
+    .addEventListener('click', confetti);
 
   // document.getElementById('arctic')
   //   .addEventListener('click', getArcticBackground);
@@ -77,13 +128,17 @@ document.addEventListener('DOMContentLoaded', () => {
   // };
 
 
-  fetch('http://www.dnd5eapi.co/api/monsters/').then(function (response) {
+  fetch('http://www.dnd5eapi.co/api/magic-items/').then(function (response) {
     // The API call was successful!
     console.log('response!', response.json());
   }).catch(function (err) {
     // There was an error
     console.warn('Something went wrong.', err);
   });
+
+  
+
+  
 
 //   // monster fetcher
 //   document.getElementById('monster')
@@ -92,6 +147,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // const setRandomMonster = () => {
   //   return `getRandomNumber()`;
   // }
+
+  
 
 
 })
