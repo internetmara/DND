0c8009f102c8d02119ef1abcea73f8e330275228
mara <ms.marafinkel@gmail.com>
2021-07-13 22:35:49 -0700

    Make NPC attributes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const firstTavernNames = ["Adorable", "Aggressive", "Exactly, Exactly", "Anxious", "Bloody", "Blushing", "Crazy", "Dead", "Dizzy", "Interesting", "Easy", "Famous", "Fantastic", "Chafing", "Flying", "Missing",  "Friendly", "Gleaming", "Grieving", "Grumpy", "Happy", "Homely", "Impossible", "Jolly", "Lonely", "Muddy", "Mushy", "Naughty", "Nervous", "Pleasant", "Puzzled", "Scary", "Sparkling", "Splendid", "Stormy", "Tasty", "Tough", "Ugly", "Vivacious", "Weary", "Witty", "Zealous", "Wrong", "Daring", "Fleeing", "Lying", "Listening", ]
   const lastTavernNames = ["walrus", "dingo", "yak", "Freak of Nature", "Weirdo", "Pinata", "Redneck", "Bourgeoisie", "Alcoholic", "Divorcee", "hog", "gopher", "opossum", "jaguar", "squirrel", "leopard", "baboon", "alpaca", "mink", "skunk", "puma", "turtle", "snake", "hartebeest", "rabbit", "seal", "shrew", "ferret", "capybara", "hippopotamus", "kitten", "rat", "badger", "puppy", "koala", "basilisk", "wombat", "kangaroo", "crow", "ox", "gila Monster", "burro", "donkey", "salamander", "lion", "rhinoceros", "pronghorn"]
   const tavernTemperment = ["Surprised", "Stressed", "Mad", "Milky", "Loved", "Lonely", "Weird", "Uncomfortable", "Touched", "Tired", "Thankful", "Sympathetic", "Smart", "Sleepy", "Silly", "Pleased", "Pessimistic", "Sick", "Shocked", "Satisfied", "Sad", "Rushed", "Restless", "Relieved", "Relaxed", "Rejuvenated", "Rejected", "Refreshed", "Recumbent", "Quixotic", "Predatory", "Peaceful", "Optimistic", "Okay", "Numb", "Giggly", "Giddy", "Not specified", "Nerdy", "Infuriated", "Indifferent", "Indescribable", "Naughty", "Morose", "Moody", "Mischievous", "Mellow", "Melancholy", "Listless", "Lethargic", "Lazy", "Jubilant", "Jealous", "Irritated", "Irate", "Impressed", "Hyper", "Hungry", "Hot", "Hopeful", "Happy", "Guilty", "Grumpy", "Groggy", "Grateful", "Dorky", "Ditzy", "Discontent", "Good", "Gloomy", "Geeky", "Full", "Frustrated", "Flirty", "Exhausted", "Excited", "Enraged", "Energetic", "Anxious", "Annoyed", "Angry", "Ecstatic", "Drunk", "Drained", "Disappointed", "Dirty", "Devious", "Determined", "Depressed", "Dark", "Cynical", "Curious", "Alone", "Aggravated", "Accomplished", "Accepted", "Crushed", "Crazy", "Crappy", "Cranky", "Content", "Confused", "Complacent", "Cold", "Chipper", "Cheerful", "Calm", "Bouncy", "Bored", "Blissful", "Blank", "Blah", "Bittersweet", "Bewildered", "Awake", "Ashamed", "Apathetic", "Amused", "Exanimate", "Envious", "Enthralled", ]
+  const NPCAppearance = ["Distinctive jewelry: earrings, necklace, circlet, bracelets", "Piercings", "Flamboyant or outlandish clothes", "Formal, clean clothes", "Ragged, dirty clothes", "Pronounced scar", "Missing teeth", "Missing fingers", "Unusual eye color (or two different colors", "Tattoos", "Birthmark", "Unusual skin color", "Bald", "Braided beard or hair", "Unusual hair color", "Nervous eye twitch", "Distinctive nose", "Distinctive posture (crooked or rigid", "Exceptionally beautiful", "Exceptionally ugly"]
+  const NPCAbilities = ["Strength - powerful, brawny, strong as an ox", "Dexterity - lythe, agile, graceful", "Constitution - hardy, hale, healthy", "Intelligence - studious, learned, inquisitive", "Wisdom - perceptive, spiritual, insightful", "Charisma - persuasive, forceful, born leader", "Strength - feeble, scrawny", "Dexterity - clumsy, fumbling", "Constitution - sickly, pale", "Intelligence - dim-witted, slow", "Wisdom - oblivious, absentminded", "Charisma - dull, boring"]
+  const NPCTalents = ["Plays a musical instrument", "Speaks several languages fluently", "Unbelievably lucky", "Perfect memory", "Great with animals", "Great with children", "Great at solving puzzles", "Great at one game", "Great at impersonations", "Draws beautifully", "Paints beautifully", "Sings beautifully", "Drinks everyone under the table", "Expert carpenter", "Expert cook", "Expert dart thrower and rock skipper", "Expert juggler", "Skilled actor and master of disguise", "Skilled dancer", "Knows thieves' cant"]
+  const NPCMannerisms = ["Prone to singing, whistling, or humming quietly", "Speaks in rhyme or some other peculiar way", "Particularly low or high voice", "Slurs words, lisps, or stutters", "Enunciates overly clearly", "Speaks loudly", "Whispers", "Uses flowery speech or long words", "Frequently uses the wrong word", "Uses colorful oaths and exclaimations", "Makes constant jokes or puns", "Prone to predictions of doom", "Fidgets", "Squints", "Stares into the distance", "Chews something", "Paces", "Taps fingers", "Bites fingernails", "Twirls hair or tugs beard"]
+  const NPCInteractions = ["Argumentative", "Arrogant", "Blustering", "Rude", "Curious", "Friendly", "Honest", "Hot tempered", "Irritable", "Ponderous", "Quiet", "Suspicious"]
+  const Alignment = ["Lawful Good", "Neutral Good", "Lawful Neutral", "Chaotic Neutral", "Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]
+  const NPCBonds = ["Dedicated to fulfilling a personal life goal", "Protective of close family members", "Protective of colleagues or compatriots", "Loyal to a benefactor, patron, or employer", "Captivated by a romantic interest", "Drawn to a special place", "Protective of a sentimental keepsake", "Protective of a valuable possession", "Out for revenge"]
+  const NPCFlawsAndSecrets = ["Forbidden love or susceptibility to romance", "Enjoys decadent pleasures", "Arrogance", "Envies another creature's possessions or station", "Overpowering greed", "Prone to rage", "Has a powerful enemy", "Specific phobia", "Shameful or scandelous history", "Secret crime or misdeed", "Possession of forbidden lore", "Foolhardy bravery"]
+  const Class = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+  const TavernType = ["Quiet, low-key bar", "Raucous dive", "Thieves' guild hangout", "Gathering place for a secret society", "Upper-class dining club", "Gambling den", "Caters to a specific race or guild", "Members-only club", "Brothel"]
   const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
   const randomNPCPic = new Array();
@@ -48,8 +58,25 @@ document.addEventListener('DOMContentLoaded', () => {
   randomTavern[6] = "dist/images/Taverns/Tavern7.jpg";
   randomTavern[7] = "dist/images/Taverns/Tavern8.jpg";
   randomTavern[8] = "dist/images/Taverns/Tavern9.jpg";
+  
+  const generateNPC = () => {
+    getRandomImage();
+    return `Name: ${firstNames[getRandomNumber(firstNames.length)]} ${lastNames[getRandomNumber(lastNames.length)]}
+    Age: ${getRandomNumber(150)}
+    Class: ${Class[getRandomNumber(Class.length)]}
+    Alignment: ${Alignment[getRandomNumber(Alignment.length)]}
+    Appearance: ${NPCAppearance[getRandomNumber(NPCAppearance.length)]}
+    Abilities: ${NPCAbilities[getRandomNumber(NPCAbilities.length)]}
+    Talent: ${NPCTalents[getRandomNumber(NPCTalents.length)]}
+    Mannerisms: ${NPCMannerisms[getRandomNumber(NPCMannerisms.length)]}
+    Interaction Trait: ${NPCInteractions[getRandomNumber(NPCInteractions.length)]}
+    Bonds: ${NPCBonds[getRandomNumber(NPCBonds.length)]}
+    Flaws and Secrets: ${NPCFlawsAndSecrets[getRandomNumber(NPCFlawsAndSecrets.length)]}`;
+  }
 
-
+  
+  
+  
   const getRandomImage = () => {
     document.getElementById('result').innerHTML = '<img src="' + randomNPCPic[getRandomNumber(randomNPCPic.length)] + '" />';
   }
@@ -110,19 +137,15 @@ document.addEventListener('DOMContentLoaded', () => {
     Learn More: https://www.dnd5eapi.co${magicItems[idx]['url']}`;
   }
 
-  const generateNPC = () => {
-    getRandomImage();
-    return `Name: ${firstNames[getRandomNumber(firstNames.length)]} ${lastNames[getRandomNumber(lastNames.length)]}
-    Age: ${getRandomNumber(150)}`;
-  }
 
   const generateTavern = () => {
     getRandomTavernPic();
     let lastTName = `${lastTavernNames[getRandomNumber(lastTavernNames.length)]}`
     return `Tavern Name: The ${firstTavernNames[getRandomNumber(firstTavernNames.length)]} ${lastTName.charAt(0).toUpperCase() + lastTName.slice(1)}
+    Type: ${TavernType[getRandomNumber(TavernType.length)]}
     Temperment: ${tavernTemperment[getRandomNumber(tavernTemperment.length)]}`;
   }
-
+  
   const generateConfetti = () => {
     var confettiSettings = { target: 'my-canvas', max: 2000, clock: 50, start_from_edge: true, rotate: true };
     var confetti = new ConfettiGenerator(confettiSettings);
